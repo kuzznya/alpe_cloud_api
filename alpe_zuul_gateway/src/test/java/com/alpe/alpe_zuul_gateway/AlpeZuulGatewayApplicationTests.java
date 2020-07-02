d36a05241e45835ad1b045e4fa1a8629d62f151a
kuzznya <ikuz_2002@mail.ru>
2020-07-02 16:53:33 +0300

    feat: Add Zuul Gateway as an alternative
    Spring Cloud Gateway needs an alternative for SAP Cloud Platform
    because it cannot be run on Tomcat

---
+++ alpe_zuul_gateway/src/test/java/com/alpe/alpe_zuul_gateway/AlpeZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpe.alpe_zuul_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpeZuulGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
