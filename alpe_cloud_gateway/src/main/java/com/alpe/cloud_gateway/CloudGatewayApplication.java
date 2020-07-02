3578a11aa1ff1b99113c05e62d8e01e02f1575bb
kuzznya <ikuz_2002@mail.ru>
2020-07-02 12:24:36 +0300

    Set up API Gateway server

--- alpe_cloud_gateway/src/main/java/com/alpe/cloud_gateway/CloudGatewayApplication.java
+++ alpe_cloud_gateway/src/main/java/com/alpe/cloud_gateway/CloudGatewayApplication.java
@@ -1,12 +1,24 @@
 package com.alpe.cloud_gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.event.InstanceRegisteredEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CloudGatewayApplication {
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @Scheduled(fixedDelay=20000)
+    public void refreshRoutes(){
+        publisher.publishEvent(new InstanceRegisteredEvent<Void>(this, null));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CloudGatewayApplication.class, args);
